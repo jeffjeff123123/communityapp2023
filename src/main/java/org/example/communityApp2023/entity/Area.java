843b19b44e74bf66ed7a25133a946b2497884786
yuqiliu <liuyq1995@gmail.com>
2023-04-15 15:39:27 -0400

    finish area entity design

--- src/main/java/org/example/communityApp2023/entity/Area.java
+++ src/main/java/org/example/communityApp2023/entity/Area.java
@@ -1,4 +1,53 @@
 package org.example.communityApp2023.entity;
 
+import java.util.Date;
+
 public class Area {
+    //use reference type to avoid initial value
+    private Integer areaId;
+    private String areaName;
+    //display the item with higher priority in front of the page
+    private Integer priority;
+    private Date createTime;
+    private Date lastEditTime;
+
+    public Integer getAreaId() {
+        return areaId;
+    }
+
+    public void setAreaId(Integer areaId) {
+        this.areaId = areaId;
+    }
+
+    public String getAreaName() {
+        return areaName;
+    }
+
+    public void setAreaName(String areaName) {
+        this.areaName = areaName;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
 }
