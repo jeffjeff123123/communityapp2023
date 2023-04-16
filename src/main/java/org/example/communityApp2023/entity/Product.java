525e983c87aaff990c5407a055afe13c908bc57c
yuqiliu <liuyq1995@gmail.com>
2023-04-16 11:21:08 -0400

    finish product entity design

--- src/main/java/org/example/communityApp2023/entity/Product.java
+++ src/main/java/org/example/communityApp2023/entity/Product.java
@@ -1,4 +1,105 @@
 package org.example.communityApp2023.entity;
 
+import java.util.Date;
+
 public class Product {
+    private Long productId;
+    private String productName;
+    private String productDesc;
+    private String imgAddr;
+    private String normalPrice;
+    private String promotionPrice;
+    private Integer priority;
+    private Integer point;
+    private Date createTime;
+    private Date lastEditTime;
+    private Integer enableStatus;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductDesc() {
+        return productDesc;
+    }
+
+    public void setProductDesc(String productDesc) {
+        this.productDesc = productDesc;
+    }
+
+    public String getImgAddr() {
+        return imgAddr;
+    }
+
+    public void setImgAddr(String imgAddr) {
+        this.imgAddr = imgAddr;
+    }
+
+    public String getNormalPrice() {
+        return normalPrice;
+    }
+
+    public void setNormalPrice(String normalPrice) {
+        this.normalPrice = normalPrice;
+    }
+
+    public String getPromotionPrice() {
+        return promotionPrice;
+    }
+
+    public void setPromotionPrice(String promotionPrice) {
+        this.promotionPrice = promotionPrice;
+    }
+
+    public Integer getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Integer priority) {
+        this.priority = priority;
+    }
+
+    public Integer getPoint() {
+        return point;
+    }
+
+    public void setPoint(Integer point) {
+        this.point = point;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public Date getLastEditTime() {
+        return lastEditTime;
+    }
+
+    public void setLastEditTime(Date lastEditTime) {
+        this.lastEditTime = lastEditTime;
+    }
+
+    public Integer getEnableStatus() {
+        return enableStatus;
+    }
+
+    public void setEnableStatus(Integer enableStatus) {
+        this.enableStatus = enableStatus;
+    }
 }
