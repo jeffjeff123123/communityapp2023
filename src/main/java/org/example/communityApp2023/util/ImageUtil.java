992ceaaee3ab2041be00ab32bfd0ea369ee4eae0
yuqiliu <liuyq1995@gmail.com>
2023-04-16 18:01:02 -0400

    update imageUtil get random file name function

--- src/main/java/org/example/communityApp2023/util/ImageUtil.java
+++ src/main/java/org/example/communityApp2023/util/ImageUtil.java
@@ -1,4 +1,13 @@
 package org.example.communityApp2023.util;
 
+import java.util.Date;
+import java.util.Random;
+
 public class ImageUtil {
+    static Random r;
+    public static String getRandomFileName(){
+        int i = r.nextInt(9999);
+        long l = System.currentTimeMillis();
+        return i + l + "";
+    }
 }
