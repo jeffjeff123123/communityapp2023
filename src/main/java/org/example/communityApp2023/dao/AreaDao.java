eb9bdfd70812e086c6cf42f5476201cfab7fd7c7
yuqiliu <liuyq1995@gmail.com>
2023-04-16 15:44:23 -0400

    debug spring-dao.xml

---
+++ src/main/java/org/example/communityApp2023/dao/AreaDao.java
@@ -0,0 +1,9 @@
+package org.example.communityApp2023.dao;
+
+import org.example.communityApp2023.entity.Area;
+
+import java.util.List;
+
+public interface AreaDao {
+    List<Area> queryArea();
+}
---
+++ src/test/java/org/example/community/dao/AreaDaoTest.java
@@ -0,0 +1,21 @@
+package org.example.community.dao;
+
+import org.example.community.service.BaseTest;
+import org.example.communityApp2023.dao.AreaDao;
+import org.example.communityApp2023.entity.Area;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class AreaDaoTest extends BaseTest {
+    @Autowired
+    private AreaDao areaDao;
+
+    @Test
+    public void testQueryArea(){
+        List<Area> areaList = areaDao.queryArea();
+        assertEquals(2, areaList.size());
+    }
+}
