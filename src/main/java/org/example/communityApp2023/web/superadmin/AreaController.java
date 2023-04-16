e05a6d3d65e051d56e1500ff3d9e4caca8c24d73
yuqiliu <liuyq1995@gmail.com>
2023-04-16 17:14:22 -0400

    update AreaController with logback

--- src/main/java/org/example/communityApp2023/web/superadmin/AreaController.java
+++ src/main/java/org/example/communityApp2023/web/superadmin/AreaController.java
@@ -3,6 +3,8 @@ package org.example.communityApp2023.web.superadmin;
 
 import org.example.communityApp2023.entity.Area;
 import org.example.communityApp2023.service.AreaService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,12 +19,15 @@ import java.util.Map;
 @Controller
 @RequestMapping("/superadmin")
 public class AreaController {
+    Logger logger = LoggerFactory.getLogger(AreaController.class);
     @Autowired
     private AreaService areaService;
 
     @RequestMapping(value = "/listarea", method = RequestMethod.GET)
     @ResponseBody
     private Map<String, Object> listArea(){
+        logger.info("===start===");
+        long startTime = System.currentTimeMillis();
         Map<String, Object> modelMqp = new HashMap<>();
         List<Area> list = new ArrayList<>();
         try{
@@ -34,7 +39,12 @@ public class AreaController {
             e.printStackTrace();
             modelMqp.put("success", false);
             modelMqp.put("errMsg", e.toString());
+            logger.error("test error!");
         }
+
+        long endTime = System.currentTimeMillis();
+        logger.debug("costTime:[{}ms]", endTime-startTime);
+        logger.info("===end===");
 
         return modelMqp;
     }
